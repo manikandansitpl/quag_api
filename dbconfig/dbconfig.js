eeb9d8472587b94c320d7131008dd71e18dace9b
maniadminm <multidiscoverserver@gmail.com>
2024-05-09 23:26:47 +0530

    add db

--- dbconfig/dbconfig.js
+++ dbconfig/dbconfig.js
@@ -2,7 +2,7 @@ const mongoose  = require("mongoose")
 
 exports.dbConfiguration=async()=>{
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
+        const conn = await mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true, useUnifiedTopology: true });
         console.log(`MongoDB Connected: ${conn.connection.host}`);
       } catch (error) {
         console.log(error);
